45e677f8a6b36c574d0d5b2ad4c7115e4af3ba04
Miroslav <housera94@gmail.com>
2017-02-07 22:31:30 +0000

    .json file and configuring index

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const restify = require('restify')
+const server = restify.createServer()
+
+server.use(restify.fullResponse())
+server.use(restify.bodyParser())
+server.use(restify.queryParser())
+server.use(restify.authorizationParser())
+
+const *** = require('***') //main module goes here
+const globals = require('.modules/globals.js')
+
+const defaultPort = 8080
+
+server.get('/', (req, res, next) => {
+    res.redirect('', next) // the first page to open if nothing is selected
+})
+
+server.get('', (req, res) => {
+
+})
+
+server.get ('', (req, res) => {
+
+})
+
+server.get ('', (req, res) => {
+
+})
+
+const port = process.env.PORT || defaultPort
+
+server.listen(port, err => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('running at: http://localhost:' + port)
+    }
+})
